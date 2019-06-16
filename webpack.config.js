f03e1ffe36e013af33c1041faddb7640e2f1181b
Juraj Garaj <juraj982@gmail.com>
2019-06-17 00:24:43 +0200

    Fixed webpack test extensions

--- webpack.config.js
+++ webpack.config.js
@@ -1,32 +1,32 @@
-const path = require("path");
+const path = require('path');
 
 module.exports = env => ({
-  entry: "./src/index.js",
-  devtool: "source-map",
-  mode: env.production ? "production" : "development",
+  entry: './src/index.js',
+  devtool: 'source-map',
+  mode: env.production ? 'production' : 'development',
   output: {
-    path: path.resolve(__dirname, "build"),
-    filename: "index.js",
-    libraryTarget: "commonjs2"
+    path: path.resolve(__dirname, 'build'),
+    filename: 'index.js',
+    libraryTarget: 'commonjs2',
   },
 
   module: {
     rules: [
       {
-        test: /\.js$/,
+        test: /\.(js|jsx)$/,
         include: __dirname,
         exclude: /(node_modules|bower_components|build)/,
         use: {
-          loader: "babel-loader",
+          loader: 'babel-loader',
           options: {
-            presets: ["@babel/preset-env"]
-          }
-        }
-      }
-    ]
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+    ],
   },
 
   externals: {
-    react: "commonjs react"
-  }
+    react: 'commonjs react',
+  },
 });
