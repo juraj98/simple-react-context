47c980592fec483bdd2fea536062bf6d870e3dce
Juraj Garaj <juraj982@gmail.com>
2019-06-17 00:22:51 +0200

    Fixed exports

--- src/index.js
+++ src/index.js
@@ -1,2 +1,5 @@
-export createContext from './createContext';
-export getContextProvider from './ContextProvider';
+import createContext from './createContext';
+import getContextProvider from './ContextProvider';
+
+export { getContextProvider };
+export default createContext;
